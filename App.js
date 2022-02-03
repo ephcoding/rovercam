60d25dea612371c80607b45da3283638a5f0e303
Ephraim <admin@ephraimsmith.dev>
2022-02-03 00:40:27 -0600

    move files to src folder

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import HomeScreen from "./src/screens/HomeScreen";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const AppStackNavigator = createStackNavigator(
+	{
+		Home: { screen: HomeScreen },
+	},
+	{
+		initialRouteName: "Home",
+		defaultNavigationOptions: {
+			title: "ROVERCAM",
+		},
+	}
+);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default createAppContainer(AppStackNavigator);
