ab94db1b0f9a93d267109c4fd4cdde7122fbb3c7
Ephraim <admin@ephraimsmith.dev>
2022-02-04 05:47:37 -0600

    add ImageBackground source path

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -8,21 +8,29 @@ import {
 } from "react-native";
 import React from "react";
 
+/**
+ * TODO:
+ * 1. cleaner way to import imgs
+ */
+
 const HomeScreen = ({ navigation }) => {
 	const handleOnPressNav = screen => navigation.navigate(screen);
 	return (
-		// <SafeAreaView style={S.safeArea}>
-		// <ImageBackground>
-		<View style={S.screen}>
-			<Pressable style={S.btn} onPress={() => handleOnPressNav("Missions")}>
-				<Text>Rover Missions</Text>
-			</Pressable>
-			<Pressable style={S.btn} onPress={() => handleOnPressNav("Options")}>
-				<Text>Photos</Text>
-			</Pressable>
-		</View>
-		// </ImageBackground>
-		// </SafeAreaView>
+		<ImageBackground
+			source={require("../../assets/mars-glowing.jpg")}
+			style={S.imgBg}
+		>
+			<SafeAreaView style={S.safeArea}>
+				<View style={S.screen}>
+					<Pressable style={S.btn} onPress={() => handleOnPressNav("Missions")}>
+						<Text>Rover Missions</Text>
+					</Pressable>
+					<Pressable style={S.btn} onPress={() => handleOnPressNav("Options")}>
+						<Text>Photos</Text>
+					</Pressable>
+				</View>
+			</SafeAreaView>
+		</ImageBackground>
 	);
 };
 
@@ -34,6 +42,12 @@ const S = StyleSheet.create({
 		borderWidth: 2,
 		paddingHorizontal: 20,
 		paddingVertical: 10,
+	},
+	imgBg: {
+		flex: 1,
+	},
+	safeArea: {
+		flex: 1,
 	},
 	screen: {
 		alignItems: "center",
