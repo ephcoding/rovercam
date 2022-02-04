fc15bde6bb81bd63ee9faec9b5b5874329208dc2
Ephraim <admin@ephraimsmith.dev>
2022-02-04 15:21:28 -0600

    create CameraList skeleton

---
+++ app/components/CamerasList.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const CamerasList = () => {
+	return (
+		<View>
+			<Text></Text>
+		</View>
+	);
+};
+
+export default CamerasList;
+
+const styles = StyleSheet.create({});
