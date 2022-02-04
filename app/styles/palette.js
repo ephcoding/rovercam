04c3e7bc2da6642767d9ea2f2c9841650c291fa2
Ephraim <admin@ephraimsmith.dev>
2022-02-04 05:17:56 -0600

    add style files

---
+++ app/styles/palette.js
@@ -0,0 +1,14 @@
+// TODO: add DK & LT variants to PRIMARY & SECONDARY palette colors
+export const PALETTE = {
+	black: "1f1f1f",
+	blue: "",
+	green: "",
+	greyDK: "",
+	greyLT: "",
+	orange: "",
+	pink: "",
+	purple: "",
+	red: "",
+	white: "e8e8e8",
+	yellow: "",
+};
