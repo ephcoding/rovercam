0ed2d0b3eeb296e0f5d800d766c3b09a4cdcffd1
Ephraim <admin@ephraimsmith.dev>
2022-02-04 21:53:16 -0600

    add nav onPress

--- app/components/NavBtn.js
+++ app/components/NavBtn.js
@@ -1,26 +1,30 @@
 import { Pressable, StyleSheet, Text, View } from "react-native";
 
-const NavBtn = ({ btnStyles, label, labelStyles, screen }) => {
-	return (
-		<Pressable key={label} style={[S.touchOpac, btnStyles]}>
-			<Text style={[S.text, labelStyles]}>{label}</Text>
-		</Pressable>
-	);
+const NavBtn = ({ btnStyles, label, labelStyles, navFunc, screen }) => {
+  return (
+    <Pressable
+      key={label}
+      onPress={() => navFunc(label)}
+      style={[S.touchOpac, btnStyles]}
+    >
+      <Text style={[S.text, labelStyles]}>{label}</Text>
+    </Pressable>
+  );
 };
 
 export default NavBtn;
 
 const S = StyleSheet.create({
-	touchOpac: {
-		borderColor: "#fff",
-		borderRadius: 10,
-		borderWidth: 2,
-		paddingHorizontal: 20,
-		paddingVertical: 10,
-		width: 150,
-	},
-	text: {
-		color: "#fff",
-		alignSelf: "center",
-	},
+  touchOpac: {
+    borderColor: "#fff",
+    borderRadius: 10,
+    borderWidth: 2,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    width: 150,
+  },
+  text: {
+    color: "#fff",
+    alignSelf: "center",
+  },
 });
