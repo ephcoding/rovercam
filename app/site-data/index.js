5cb022d514aa14f5e9e3d490f6cc730bab92e57e
Ephraim <admin@ephraimsmith.dev>
2022-02-06 14:50:56 -0600

    move rover-related data to constants/

--- app/site-data/index.js
+++ app/site-data/index.js
@@ -1,5 +1,2 @@
 export * from "./fake-manifest-data";
 export * from "./nasa-api-list";
-export * from "./rover-cameras";
-export * from "./rover-image-paths";
-export * from "./rover-names";
--- app/site-data/rover-image-paths.js
+++
@@ -1,6 +0,0 @@
-export const IMG_PATHS = {
-	Curiosity: require("../../assets/img/curiosity-up-close.jpg"),
-	Opportunity: require("../../assets/img/opportunity.jpg"),
-	Perseverance: require("../../assets/img/perseverance.jpg"),
-	Spirit: require("../../assets/img/spirit.jpg"),
-};
