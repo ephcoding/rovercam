0d5576fe7c85506c2cf178d4e97ff2a6f35d8cbd
Ephraim <admin@ephraimsmith.dev>
2022-02-11 19:37:43 -0600

    add SearchScreen Switch

--- app/screens/SearchPhotosScreen.js
+++ app/screens/SearchPhotosScreen.js
@@ -1,9 +1,12 @@
+import { useState } from "react";
 import { ImageBackground, SafeAreaView, StyleSheet } from "react-native";
-import { ButtonGroup, Text } from "react-native-elements";
+import { ButtonGroup, Switch, Text } from "react-native-elements";
 
 const SearchPhotosScreen = ({ navigation, route }) => {
 	console.log(">> NAVIGATION >>\n", navigation);
 	console.log(">> ROUTE >>\n", route);
+
+	const [isSwitched, setIsSwitched] = useState(false);
 
 	return (
 		<SafeAreaView style={S.safeArea}>
@@ -13,7 +16,14 @@ const SearchPhotosScreen = ({ navigation, route }) => {
 				style={S.imgContainer}
 				source={route.params.img_url}
 			>
-				<Text>{JSON.stringify(route.params)}</Text>
+				<Switch
+					onValueChange={() => setIsSwitched(!isSwitched)}
+					color={"#ff0"}
+					ios_backgroundColor={"#777"}
+					thumbColor={"#f60"}
+					trackColor={{ false: "#777", true: "#0f0" }}
+					value={isSwitched}
+				/>
 			</ImageBackground>
 		</SafeAreaView>
 	);
@@ -23,7 +33,9 @@ export default SearchPhotosScreen;
 
 const S = StyleSheet.create({
 	imgContainer: {
+		alignItems: "center",
 		flex: 1,
+		justifyContent: "center",
 	},
 	imgStyle: {
 		flex: 1,
