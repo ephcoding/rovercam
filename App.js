516ad8b3d090abd5fc87d398efea28bea4daa21c
Ephraim <admin@ephraimsmith.dev>
2022-03-15 03:53:27 -0500

    add NativeBaseProvider to App.js

--- App.js
+++ App.js
@@ -14,80 +14,83 @@ import SOLPickerScreen from "./app/screens/SOLPickerScreen";
 // -----
 import { RNE_THEME } from "./app/styles/themes";
 import { ThemeProvider } from "react-native-elements";
+import { NativeBaseProvider } from "native-base";
 // -----
 const queryClient = new QueryClient();
 
 export default App = () => {
 	return (
 		<QueryClientProvider client={queryClient}>
-			<ThemeProvider theme={RNE_THEME}>
-				<SafeAreaProvider>
-					<NavigationContainer>
-						<Stack.Navigator
-							initialRouteName='Home'
-							screenOptions={{
-								headerStyle: {
-									backgroundColor: "#000",
-								},
-								headerTintColor: "#fff",
-								headerTitleStyle: {
-									color: "#fff",
-									fontSize: 20,
-									fontWeight: "bold",
-								},
-							}}
-						>
-							<Stack.Screen
-								name='Home'
-								component={HomeScreen}
-								options={{ title: "ROVERCAM" }}
-							/>
-							<Stack.Screen
-								name='CameraPicker'
-								component={CameraPickerScreen}
-								options={({ route }) => ({
-									title: "CAMERAS",
-								})}
-							/>
-							<Stack.Screen
-								name='DatePicker'
-								component={DatePickerScreen}
-								options={({ route }) => ({
-									title: "EARTH DATE",
-								})}
-							/>
-							<Stack.Screen
-								name='DisplayPhotos'
-								component={DisplayPhotosScreen}
-								options={({ route }) => ({
-									title: route.params.paramType,
-								})}
-							/>
-							{/* <Stack.Screen
+			<NativeBaseProvider>
+				<ThemeProvider theme={RNE_THEME}>
+					<SafeAreaProvider>
+						<NavigationContainer>
+							<Stack.Navigator
+								initialRouteName='Home'
+								screenOptions={{
+									headerStyle: {
+										backgroundColor: "#000",
+									},
+									headerTintColor: "#fff",
+									headerTitleStyle: {
+										color: "#fff",
+										fontSize: 20,
+										fontWeight: "bold",
+									},
+								}}
+							>
+								<Stack.Screen
+									name='Home'
+									component={HomeScreen}
+									options={{ title: "ROVERCAM" }}
+								/>
+								<Stack.Screen
+									name='CameraPicker'
+									component={CameraPickerScreen}
+									options={({ route }) => ({
+										title: "CAMERAS",
+									})}
+								/>
+								<Stack.Screen
+									name='DatePicker'
+									component={DatePickerScreen}
+									options={({ route }) => ({
+										title: "EARTH DATE",
+									})}
+								/>
+								<Stack.Screen
+									name='DisplayPhotos'
+									component={DisplayPhotosScreen}
+									options={({ route }) => ({
+										title: route.params.paramType,
+									})}
+								/>
+								{/* <Stack.Screen
 								name='LatestPhotos'
 								component={LatestPhotosScreen}
 								options={({ route }) => ({
 									title: `LATEST ${route.params.rover.toUpperCase()} PHOTOS`,
 								})}
 							/> */}
-							<Stack.Screen
-								name='Rover'
-								component={RoverScreen}
-								options={({ route }) => ({
-									title: route.params.rover.toUpperCase(),
-								})}
-							/>
-							<Stack.Screen
-								name='SOLPicker'
-								component={SOLPickerScreen}
-								options={({ route }) => ({
-									title: "MARTIAN SOL",
-								})}
-							/>
-						</Stack.Navigator>
-					</NavigationContainer>
-				</SafeAreaProvider>
-			</ThemeProvider>
+								<Stack.Screen
+									name='Rover'
+									component={RoverScreen}
+									options={({ route }) => ({
+										title: route.params.rover.toUpperCase(),
+									})}
+								/>
+								<Stack.Screen
+									name='SOLPicker'
+									component={SOLPickerScreen}
+									options={({ route }) => ({
+										title: "MARTIAN SOL",
+									})}
+								/>
+							</Stack.Navigator>
+						</NavigationContainer>
+					</SafeAreaProvider>
+				</ThemeProvider>
+			</NativeBaseProvider>
 		</QueryClientProvider>
 	);
 };
