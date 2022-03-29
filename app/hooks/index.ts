8902a6cff03105e924b41ee77559a024928490b5
Ephraim <admin@ephraimsmith.dev>
2022-03-29 11:00:19 -0500

    delete. delete. delete.

--- app/hooks/index.ts
+++ app/hooks/index.ts
@@ -1,2 +1,14 @@
-export * from "./useFetchPhotos";
-export * from "./useFetchManifest";
+import { useQuery } from "react-query";
+import * as api from "../mars-photos-api/requests";
+
+export const useManifest = rover => {
+	return useQuery(["manifest", rover], () => api.fetchManifest(rover));
+};
+export const useLatestPhotos = rover => {
+	return useQuery(["latestPhotos", rover], () => api.fetchLatestPhotos(rover));
+};
+export const usePhotosByParam = ({ rover, param, value }) => {
+	return useQuery(["photos", rover], () =>
+		api.fetchPhotosByParam(rover, param, value)
+	);
+};
--- app/hooks/useFetchManifest.ts
+++
@@ -1,6 +0,0 @@
-import { fetchManifest } from "../mars-photos-api";
-import { useQuery } from "react-query";
-
-export const useFetchManifest = rover => {
-	return useQuery(["manifest", rover], () => fetchManifest(rover));
-};
